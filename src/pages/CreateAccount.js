a4335d318afd8d471bd4ba7a6a9b55dd6c3e1d24
Felipe Herrera Tapia <fherrera.main@gmail.com>
2023-03-01 22:17:55 -0300

    amend

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import leftArrow from '../assets/left-arrow.svg';
 
 const CreateAccount = () => {
   return (
     <div>
-      Crear cuenta
+    <h1 className='view-title'> <span><img src={leftArrow} /></span> Crear cuenta</h1>
       <Outlet></Outlet>
     </div>
   )
