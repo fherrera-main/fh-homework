e797cd9f8b752507d0d4c99f954476d2f08235d2
Nicolas Reyes <nicolas.r.suarez@gmail.com>
2023-03-03 03:07:49 -0300

    recovery mobile/web views

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Row  className='app-row-main-container justify-content-sm-center m-0'>
       <Col xs='12' className='app-col-main-container p-0'>
       { 
-        validar() && <nav><TopBar /></nav>
+        <nav><TopBar /></nav>
       }
       </Col>
       <Col xs='12' className='app-col-router-container p-0'>
