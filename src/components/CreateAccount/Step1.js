1fc45815803a95bf205d9e6b93f867222b6c2570
Felipe Herrera Tapia <fherrera.main@gmail.com>
2023-03-03 03:59:26 -0300

    avance a comportamiento de stepper con rutas amend estrucutura base

--- src/components/CreateAccount/Step1.js
+++ src/components/CreateAccount/Step1.js
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
+import { useOutletContext } from "react-router-dom";
+import BackToLink from '../common/BackToLink'
 
-const Step1 = () => {
+const Step1 = ({setNewStep}) => {
+  const [changeStepHandler] = useOutletContext();
+
+  useEffect(() => {
+    console.log("Me actualice")
+    changeStepHandler(1)
+  });
+  
   return (
     <div>
       Paso 1
+      <BackToLink text='Ir a paso 2' reditectTo={'/create-account/step-2'}/>
     </div>
   )
 }
--- src/components/common/BackToLink.js
+++ src/components/common/BackToLink.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import styles from './BackToLink.module.css';
 
-const BackToLink = () => {
+const BackToLink = ({reditectTo, text = 'Default text'}) => {
     return (
         <>
             <span className={styles.backLinkSpan}>
                 <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd" d="M15.8333 10C15.8333 10.3452 15.538 10.625 15.1736 10.625H7.53021L10.3623 13.3081C10.62 13.5521 10.62 13.9479 10.3623 14.1919C10.1047 14.436 9.68698 14.436 9.42934 14.1919L5.47101 10.4419C5.21337 10.1979 5.21337 9.80214 5.47101 9.55806L9.42934 5.80806C9.68698 5.56398 10.1047 5.56398 10.3623 5.80806C10.62 6.05214 10.62 6.44787 10.3623 6.69194L7.53021 9.375H15.1736C15.538 9.375 15.8333 9.65482 15.8333 10Z" fill="white" />
                 </svg>
-                <Link to="/login" className={'mt-5 ' + styles.backLink}>Volver al login</Link>
+                <Link to={reditectTo} className={'mt-5 ' + styles.backLink}> {text}</Link>
             </span>
         </>
     )
