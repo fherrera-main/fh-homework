e42f5bc7cd506eed5e9dbba4346a683724637de3
Nicolas Reyes <nicolas.r.suarez@gmail.com>
2023-03-03 21:33:16 -0300

    Recovery last details

--- src/pages/Recovery.js
+++ src/pages/Recovery.js
@@ -8,25 +8,25 @@ const Recovery = () => {
   return (
     <Container className={styles.formContainer}>
       <Row>
-        <Col>
+        <Col xs={12}>
           <h1 className={styles.title}>Restaurar Contraseña</h1>
         </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Input LeftSection={false} LabelText='Nueva Contraseña' InputType='password' IconName='eye' IconColor='#fff' />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Input LeftSection={false} LabelText='Confirmar Contraseña' InputType='password' IconName='eye' IconColor='#fff' />
-        </Col>
-      </Row>
-      <Row>
-        <Col className={styles.button}>
+        <Row>
+          <Col xs={12} className={styles.input}>
+            <Input LeftSection={false} LabelText='Nueva Contraseña' InputType='password' IconName='eye' IconColor='#fff' />
+          </Col>
+          <Col xs={12} className={styles.input}>
+            <Input LeftSection={false} LabelText='Confirmar Contraseña' InputType='password' IconName='eye' IconColor='#fff' />
+          </Col>
+        </Row>
+        <Col xs={12} className={styles.button}>
           <Button text={'Restablecer contraseña'} />
         </Col>
-        <Col xs={12} className={styles.back}><BackToLink/></Col>
+        <Col xs={12} className={styles.back}>
+          <BackToLink text='Volver al login'/>
+        </Col>
+
+
       </Row>
     </Container>
   )
