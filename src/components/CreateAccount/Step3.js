42a3d79bbd3872134a7020478e234b2d4c02f484
Nicolas Reyes <nicolas.r.suarez@gmail.com>
2023-03-06 17:07:01 -0300

    advances step 3

--- src/components/CreateAccount/Step3.js
+++ src/components/CreateAccount/Step3.js
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 import { useOutletContext } from "react-router-dom";
-import BackToLink from '../common/BackToLink'
+import BackToLink from '../common/BackToLink';
+import { Container, Row, Col } from 'react-bootstrap';
+import styles from '../CreateAccount/Step3.module.css';
+import Button from '../../components/common/Button.js';
+import Input from '../../components/common/Input';
 
 const Step3 = () => {
   const [changeStepHandler] = useOutletContext();
@@ -11,10 +15,27 @@ const Step3 = () => {
   });
 
   return (
-    <div>
+    <Container>
+      <Row>
+        <Col>
+          <h2 className={styles.subtitle}>Por último, indícanos los datos de tu cuenta</h2>
+        </Col>
+        <Col xs={12}>
+          <Input LabelText='Correo' LeftSection={false} RightSection={false}/>
+        </Col>
+        <Col xs={12}>
+          <Input LabelText='Contraseña' LeftSection={false} IconName='eye' IconColor='#fff'/>
+        </Col>
+        <Col xs={12}>
+          <Input LabelText='Confirmar contraseña' LeftSection={false} IconName='eye' IconColor='#fff'/>
+        </Col>
+        <Col className={styles.button}>
+          <Button text='Crear cuenta'/>
+        </Col>
+      </Row>
       Paso 3
       <BackToLink reditectTo={'/create-account/step-1'}/>
-    </div>
+    </Container>
   )
 }
 
