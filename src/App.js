620cf8c524e4876dfd93250017fb3958dea9f622
Felipe Herrera Tapia <fherrera.main@gmail.com>
2023-03-27 22:03:59 -0300

    login completo, cambio de diseño y logo

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 
 function App() {
+  const isLocationLogin = window.location.href.includes('/login');
   return (
     <Provider store={store}>
       <nav>
-          <TopBar />
+        {
+          !isLocationLogin && <TopBar />
+        }
       </nav>
       <Row className='app-row-main-container justify-content-sm-center m-0'>
         <Col xs='12' className='app-col-router-container p-0'>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,53 @@
 import Button from "../components/common/Button";
 import Input from '../components/common/Input';
-import {Container,Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import styles from '../components/common/Login/Login.module.css';
 import loginIcon from '../assets/homework-icon.svg';
+import { useState } from 'react';
+import HomeworkLogoLamp from '../assets/homework-logo-lamp.svg'
 
 const Login = () => {
-  const log = () => {
+  const [form, setForm] = useState({})
+
+  const handleChange = (e) => {
+    const { name, value, type } = e.target;
+
+    setForm((prevValues) => ({
+      ...prevValues,
+      [name]: type === 'checkbox' ? e.target.checked : value,
+    }));
+  };
+
+  const handleClick = () => {
+    const {email, password} = form;
     const USER_DATA = {
-      name: 'hola'
+      email,
+      password
     }
-    localStorage.setItem('USER_DATA', USER_DATA)
+    localStorage.setItem('USER_DATA', JSON.stringify(USER_DATA))
+    //navigate('/home')
   }
   return (
-    <Container className={styles.ContainerToCenter}>
-      <Row className={'' + styles.mainContainer}>
-        <Col xs={12} className={'' + styles.logoContainer}>
-          <img src={loginIcon} alt=''></img>
+    <div className={' ' + styles.loginFormContainer}>
+      <Row className="rowContainerLogin">
+        <Col sm={12}>
+          <img className="mx-auto d-block mt-5" src={HomeworkLogoLamp} />
         </Col>
-        <Col xs={12} className={'' + styles.inputContainer}>
-          <Input LeftSection={false} LabelText='Correo' InputType='email' IconColor='#fff' />
-          <Input LeftSection={false} LabelText='Contraseña' InputType='password' IconName='eye' IconColor='#fff' />
-        </Col>
-        <Col xs={12} className={'' + styles.recoveryLink}>
-          <p>¿Olvidaste tu contraseña?</p>
-        </Col>
-        <Col xs={12} className={'' + styles.buttonLogin}>
-          <Button onClick={log} text={'Iniciar sesión'} />
+        <Col className="mt-5" sm={12}>
+            <Row>
+              <Col sm={12} md={6}>
+                <Input handleChange={handleChange} inputName='email' LabelText='Correo' LeftSection={false} RightSection={false} />
+              </Col>
+              <Col sm={12} md={6}>
+                <Input handleChange={handleChange} inputName='password' className LeftSection={false} LabelText='Contraseña' InputType='password' IconName='eye' IconColor='#fff' />
+              </Col>
+              <Col className="mt-3" sm={12} md={4} >
+                <Button text='Iniciar sesión' onClick={handleClick} />
+              </Col>
+            </Row>
         </Col>
       </Row>
-    </Container>
+    </div>
 
   )
 }
